b4a1e09c6c7289a44d446f9b960c7fc37e80a065
Cesca C <cescanyc@msn.com>
2020-03-25 20:59:15 -0400

    update 1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class ToDoList extends Component {
+
+constructor(){
+  super();
+  this.state = {
+    todos: [],
+    currentToDo: "",
+  }
 }
 
-export default App;
+//this is where we code our methods
+
+addItem = (event) => {
+  event.preventDefault();
+  console.log("The addItem method executed");
+}
+
+handleChange = (event)=> {
+  console.log(event.target.value);
+}
+
+render() {
+  return (
+  <div className="container">
+      <form onSubmit={this.addItem}>
+        <label htmlFor="taskName">Task Name:</label>
+        <input onChange={this.handleChange} name="taskName" type="text" placeholder="Add todo here!"/>
+        <button type="submit">Add Task</button>
+      </form>
+
+    /*here is where we build the list with a delete button
+
+  </div>
+
+
+
+
+  )
+
+}
+
+}
+
+export default ToDoList;
